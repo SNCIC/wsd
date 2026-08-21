509341db08a9ba06a9c67e2ada698d90b28a8a66
Gavin <963645882@qq.com>
2026-08-21 10:04:56 +0800

    chore: remove obsolete local helper scripts

--- start-odoo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Odoo 19 - mes
-echo Starting Odoo (mes) ...
-echo Browser: http://localhost:8069   (admin / admin)
-cd /d D:\odoo19e20250921-f
-D:\ProgramDatas\Anaconda\envs\odoo19\python.exe D:\odoo19e20250921-f\odoo-bin -c D:\wsd\odoo.local.conf -d mes
-pause
